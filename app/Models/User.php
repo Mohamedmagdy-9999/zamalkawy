1a5162b2a0b0464fea48760897137c6f042d4134
Mohamedmagdy-9999 <mohamedmagdy111996@gmail.com>
2026-04-14 15:45:25 +0200

    update

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,37 +8,90 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Database\Eloquent\SoftDeletes;
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable,SoftDeletes;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
+    protected $guarded = [];
+    protected $appends = ['image_url','country_name','governorate_name','area_name','gender_name'];
     protected $hidden = [
         'password',
         'remember_token',
+        'test',
     ];
+    
+    protected $dates = ['deleted_at'];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    // ✅ مطلوب من JWT
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    // ✅ مطلوب من JWT
+    public function getJWTCustomClaims()
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'phone' => $this->phone,
+            'image' => $this->image_url,
+            'country_name' => $this->country ? $this->country->name_ar : null,
+            'governorate_name' => $this->governorate ? $this->governorate->name_ar : null,
+            'area_name' => $this->area ? $this->area->name_ar : null,
+            'gender_name' => $this->gender ? $this->gender->name_ar : null,
+            
+        ];
+    }
+
+    public function getImageUrlAttribute()
+    {
+        if ($this->image) {
+            return asset('users/' . $this->image);
+        }
+        return null;
+    }
+
+    public function country()
+    {
+        return $this->belongsTo(Country::class,'country_id');
+    }
+
+    public function getCountryNameAttribute()
+    {
+        return $this->country ? $this->country->name_ar : null;
+    }
+
+    public function governorate()
+    {
+        return $this->belongsTo(Governorate::class,'governorate_id');
+    }
+
+    public function getGovernorateNameAttribute()
+    {
+        return $this->governorate ? $this->governorate->name_ar : null;
+    }
+
+    public function area()
+    {
+        return $this->belongsTo(Area::class,'area_id');
+    }
+
+    public function getAreaNameAttribute()
+    {
+        return $this->area ? $this->area->name_ar : null;
+    }
+
+    public function gender()
+    {
+        return $this->belongsTo(Gender::class,'gender_id');
+    }
+
+    public function getGenderNameAttribute()
+    {
+        return $this->gender ? $this->gender->name_ar : null;
+    }
+
+    
 }
