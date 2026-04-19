bfee4cc56c665a57e330279dd0e9fcea2becf1c3
Mohamedmagdy-9999 <mohamedmagdy111996@gmail.com>
2026-04-19 13:19:59 +0200

    admin dashboard

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -8,4 +8,43 @@ use Illuminate\Database\Eloquent\Model;
 class Blog extends Model
 {
     use HasFactory;
+    protected $guarded = [];
+
+    protected $appends = ['image_url','title','category_name','desc'];
+
+    public function getImageUrlAttribute()
+    {
+        return asset('blog/' . $this->image);
+    }
+
+    public function getTitleAttribute()
+    {
+        $locale = app()->getLocale();
+
+        return $locale == 'ar'
+            ? $this->title_ar
+            : $this->title_en;
+    }
+
+
+
+    public function getDescAttribute()
+    {
+        $locale = app()->getLocale();
+
+        return $locale == 'ar'
+            ? $this->desc_ar
+            : $this->desc_en;
+    }
+
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class , 'category_id');
+    }
+
+    public function getCategoryNameAttribute()
+    {
+         return $this->category ? $this->category->name : null;
+    }
 }
