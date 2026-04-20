3b9a073f05f1a4951786909b7dc58ddc8cbf99ac
Mohamedmagdy-9999 <mohamedmagdy111996@gmail.com>
2026-04-20 14:14:26 +0200

    add comment

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,10 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     protected $guarded = [];
+    protected $hidden = ['user_id','updated_at'];
+    protected $appends = ['user_name','user_image_url'];
 
     public function user()
     {
         return $this->belongsTo(User::class,'user_id');
+    }
+
+    public function getUserNameAttribute()
+    {
+         return $this->user ? $this->user->first_name .' '. $this->user->last_name : null;
+    }
+
+    public function getUserImageUrlAttribute()
+    {
+         return $this->user ? $this->user->image_url : null;
     }
 
     public function post()
