b5296b6a0bff49f61b2088bb69db3f6a80c12ca0
Mohamedmagdy-9999 <mohamedmagdy111996@gmail.com>
2026-05-10 16:27:22 +0200

    delete account api

--- routes/api.php
+++ routes/api.php
@@ -32,7 +32,7 @@ Route::prefix('v-mobile')->group(function () {
             Route::get('user_profile_completion','MobileApiController@user_profile_completion');
             Route::get('check','MobileApiController@check');
             Route::post('complete_profile','MobileApiController@complete_profile');
-            Route::post('delete_user', 'AuthApiController@delete_user');
+            Route::post('delete_user', 'MobileApiController@delete_user');
 
             Route::get('categories','MobileApiController@categories');
             Route::get('categoryBlogs/{id}','MobileApiController@categoryBlogs');
